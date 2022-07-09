db321133c6f550733d8ab58f96d7315c61434496
bharatpathak1998 <bharatpathak099@gmail.com>
2022-07-09 23:08:17 +0530

    initial commit

---
+++ LeapYear.java
@@ -0,0 +1,22 @@
+package basiccoreprograms;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a year to check leap year or not");
+	        int year = sc.nextInt();
+		
+		if(year%100==0 && year%400==0 || year%100!=0 && year%4==0)
+		{
+			System.out.println(+year+ " is leap year");
+		}
+		else
+		{
+			System.out.println(+year+ " is not a leap year");
+		}
+	}
+}
