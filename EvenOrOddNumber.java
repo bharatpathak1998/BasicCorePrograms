433e36f837bb93dcb708c0e61fb4115aa71b3f95
bharatpathak1998 <bharatpathak099@gmail.com>
2022-07-10 14:16:11 +0530

    initial commit

---
+++ EvenOrOddNumber.java
@@ -0,0 +1,23 @@
+package basiccoreprograms;
+
+import java.util.Scanner;
+
+public class EvenOrOddNumber {
+
+    public static void main(String[] args) {
+		
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num %2 == 0){
+            System.out.println("Number is Even");
+        }
+        else
+        {
+            System.out.println("Number is Odd");
+        }
+
+    }
+
+}
